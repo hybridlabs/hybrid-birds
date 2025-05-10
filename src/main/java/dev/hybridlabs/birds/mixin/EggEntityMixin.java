d292c4299233a2c20975db57840777eb8f1a6c3e
Daniel Orr <orrdaniel73@gmail.com>
2025-05-10 12:21:55 +0100

    feat(eggs): make custom eggs spawn their custom entities

--- src/main/java/dev/hybridlabs/birds/mixin/EggEntityMixin.java
+++ src/main/java/dev/hybridlabs/birds/mixin/EggEntityMixin.java
@@ -1,22 +1,29 @@
 package dev.hybridlabs.birds.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import dev.hybridlabs.birds.entity.HybridBirdsEntityTypes;
-import dev.hybridlabs.birds.entity.bird.ChickEntity;
+import dev.hybridlabs.birds.item.CustomEggItem;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.projectile.thrown.EggEntity;
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(EggEntity.class)
-public abstract class EggEntityMixin {
+public abstract class EggEntityMixin extends ThrownItemEntity {
+    private EggEntityMixin(EntityType<? extends ThrownItemEntity> type, World world) {
+        super(type, world);
+    }
 
-    @Inject(method = "onCollision", at = @At("HEAD"), cancellable = true)
-    private void onCollision(HitResult hitResult, CallbackInfo ci) {
-        EggEntity eggEntity = (EggEntity) (Object) this;
-        World world = eggEntity.getWorld();
+    @WrapMethod(method = "onCollision")
+    private void onCollision(HitResult result, Operation<Void> original) {
+        World world = this.getWorld();
         if (!world.isClient) {
             if (world.random.nextInt(8) == 0) {
                 int i = 1;
@@ -25,19 +32,30 @@ public abstract class EggEntityMixin {
                 }
 
                 for (int j = 0; j < i; ++j) {
-                    ChickEntity chickEntity = HybridBirdsEntityTypes.INSTANCE.getCHICK().create(world);
-                    if (chickEntity != null) {
-                        chickEntity.setBreedingAge(-24000);
-                        chickEntity.refreshPositionAndAngles(eggEntity.getX(), eggEntity.getY(), eggEntity.getZ(), eggEntity.getYaw(), 0.0F);
-                        world.spawnEntity(chickEntity);
+                    EntityType<?> childType = getTypeForChild();
+                    Entity childEntity = childType.create(world);
+                    if (childEntity != null) {
+                        if (childEntity instanceof PassiveEntity passiveChildEntity) {
+                            passiveChildEntity.setBreedingAge(-24000);
+                        }
+
+                        childEntity.refreshPositionAndAngles(this.getX(), this.getY(), this.getZ(), this.getYaw(), 0.0F);
+                        world.spawnEntity(childEntity);
                     }
                 }
             }
 
-            world.sendEntityStatus(eggEntity, (byte) 3);
-            eggEntity.discard();
+            world.sendEntityStatus(this, (byte) 3);
+            this.discard();
         }
+    }
 
-        ci.cancel();
+    @Unique
+    private EntityType<?> getTypeForChild() {
+        ItemStack stack = this.getItem();
+        if (stack.getItem() instanceof CustomEggItem eggItem) {
+            return eggItem.getType();
+        }
+        return HybridBirdsEntityTypes.INSTANCE.getCHICK();
     }
 }
