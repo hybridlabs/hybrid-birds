ca403f5afe5cff95dd0720345adb2206dd2f17ae
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-08-11 20:57:23 +0300

    Birds turn their heads now, need to fix pivots

--- src/client/kotlin/dev/hybridlabs/birds/model/entity/bird/HybridBirdsEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/birds/model/entity/bird/HybridBirdsEntityModel.kt
@@ -3,6 +3,9 @@ package dev.hybridlabs.birds.model.entity.bird
 import dev.hybridlabs.birds.HybridBirds
 import dev.hybridlabs.birds.entity.bird.HybridBirdsBirdEntity
 import net.minecraft.util.Identifier
+import net.minecraft.util.math.MathHelper
+import software.bernie.geckolib.constant.DataTickets
+import software.bernie.geckolib.core.animation.AnimationState
 import software.bernie.geckolib.model.GeoModel
 
 abstract class HybridBirdsEntityModel<T: HybridBirdsBirdEntity> (private val id: String) : GeoModel<T>() {
@@ -16,5 +19,16 @@ abstract class HybridBirdsEntityModel<T: HybridBirdsBirdEntity> (private val id:
 
     override fun getAnimationResource(animatable: T): Identifier {
         return Identifier(HybridBirds.MOD_ID, "animations/$id.animation.json")
+    }
+
+    override fun setCustomAnimations(animatable: T, instanceId: Long, animationState: AnimationState<T>) {
+        val head = animationProcessor.getBone("head")
+
+        if (head != null) {
+            val entityData = animationState.getData(DataTickets.ENTITY_MODEL_DATA)
+
+            head.rotX = entityData.headPitch() * MathHelper.RADIANS_PER_DEGREE
+            head.rotY = entityData.netHeadYaw() * MathHelper.RADIANS_PER_DEGREE
+        }
     }
 }
