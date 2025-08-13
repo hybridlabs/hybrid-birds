aa258935f9fe8e3a677d07637d9201ab5b5c4505
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-08-13 11:11:13 +0300

    Turkeys can be fattened when fed wheat

--- src/client/kotlin/dev/hybridlabs/birds/model/entity/bird/TurkeyEntityModel.kt
+++ src/client/kotlin/dev/hybridlabs/birds/model/entity/bird/TurkeyEntityModel.kt
@@ -1,5 +1,20 @@
 package dev.hybridlabs.birds.model.entity.bird
 
-import dev.hybridlabs.birds.entity.bird.HybridBirdsBirdEntity
+import dev.hybridlabs.birds.entity.bird.TurkeyEntity
+import net.minecraft.util.Identifier
 
-class TurkeyEntityModel : HybridBirdsEntityModel<HybridBirdsBirdEntity>("turkey")
+class TurkeyEntityModel : HybridBirdsEntityModel<TurkeyEntity>("turkey") {
+
+    private val NORMAL_MODEL = Identifier("hybrid-birds", "geo/turkey.geo.json")
+    private val FAT_MODEL = Identifier("hybrid-birds", "geo/rooster.geo.json")
+    private val STUFFED_MODEL = Identifier("hybrid-birds", "geo/swan.geo.json")
+
+    override fun getModelResource(animatable: TurkeyEntity): Identifier {
+        return when (animatable.getStuffingLevel()) {
+            0 -> NORMAL_MODEL
+            1 -> FAT_MODEL
+            2 -> STUFFED_MODEL
+            else -> NORMAL_MODEL
+        }
+    }
+}
--- src/main/kotlin/dev/hybridlabs/birds/entity/bird/TurkeyEntity.kt
+++ src/main/kotlin/dev/hybridlabs/birds/entity/bird/TurkeyEntity.kt
@@ -10,16 +10,22 @@ import net.minecraft.entity.ai.pathing.EntityNavigation
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.damage.DamageSource
+import net.minecraft.entity.data.DataTracker
+import net.minecraft.entity.data.TrackedData
+import net.minecraft.entity.data.TrackedDataHandlerRegistry
 import net.minecraft.entity.mob.WaterCreatureEntity
 import net.minecraft.entity.passive.PassiveEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import net.minecraft.nbt.NbtCompound
 import net.minecraft.recipe.Ingredient
 import net.minecraft.registry.tag.ItemTags
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Hand
 import net.minecraft.world.World
 import net.minecraft.world.event.GameEvent
 
@@ -41,7 +47,7 @@ class TurkeyEntity(entityType: EntityType<out TurkeyEntity>, world: World) :
     override fun initGoals() {
         goalSelector.add(0, SwimGoal(this))
         goalSelector.add(0, EscapeDangerGoal(this, 0.6))
-        goalSelector.add(1, TemptGoal(this, 0.6, BREEDING_INGREDIENT, false))
+        goalSelector.add(1, TemptGoal(this, 0.6, Ingredient.fromTag(ItemTags.VILLAGER_PLANTABLE_SEEDS), false))
         goalSelector.add(2, AnimalMateGoal(this, 0.5))
         goalSelector.add(2, WanderAroundGoal(this, 0.5))
         goalSelector.add(2, LookAroundGoal(this))
@@ -62,6 +68,65 @@ class TurkeyEntity(entityType: EntityType<out TurkeyEntity>, world: World) :
         }
     }
 
+    private fun isFatteningItem(stack: ItemStack): Boolean {
+        return stack.isOf(Items.WHEAT)
+    }
+
+    fun getStuffingLevel(): Int {
+        return dataTracker.get(STUFFING_LEVEL)
+    }
+
+    private fun setStuffingLevel(level: Int) {
+        val newSpeed = level.coerceIn(0, 2)
+        dataTracker.set(STUFFING_LEVEL, newSpeed)
+
+        val speed = when (newSpeed) {
+            0 -> 0.5
+            1 -> 0.45
+            2 -> 0.4
+            else -> 0.5
+        }
+
+        getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED)?.baseValue = speed
+    }
+
+    override fun interactMob(player: PlayerEntity, hand: Hand?): ActionResult {
+        val itemStack = player.getStackInHand(hand)
+
+        if (isFatteningItem(itemStack)) {
+            if (!world.isClient) {
+                val currentFatness = getStuffingLevel()
+                if (currentFatness < 2) {
+                    setStuffingLevel(currentFatness + 1)
+                    playSound(SoundEvents.ENTITY_FOX_EAT, 1.0f, 1.0f)
+                    if (!player.abilities.creativeMode) {
+                        itemStack.decrement(1)
+                    }
+                }
+            }
+            return ActionResult.SUCCESS
+        }
+        return super.interactMob(player, hand)
+    }
+
+
+    override fun initDataTracker() {
+        super.initDataTracker()
+        dataTracker.startTracking(STUFFING_LEVEL, 0)
+    }
+
+    override fun writeCustomDataToNbt(nbt: NbtCompound) {
+        super.writeCustomDataToNbt(nbt)
+        nbt.putInt("StuffingLevel", getStuffingLevel())
+    }
+
+    override fun readCustomDataFromNbt(nbt: NbtCompound) {
+        super.readCustomDataFromNbt(nbt)
+        setStuffingLevel(nbt.getInt("FatnessLevel"))
+    }
+
+    // region SFX
+
     override fun getAmbientSound(): SoundEvent {
         return HybridBirdsSoundEvents.TURKEY_AMBIENT
     }
@@ -73,6 +138,8 @@ class TurkeyEntity(entityType: EntityType<out TurkeyEntity>, world: World) :
     override fun getDeathSound(): SoundEvent {
         return HybridBirdsSoundEvents.TURKEY_DIE
     }
+
+    // endregion
 
     override fun isBreedingItem(stack: ItemStack?): Boolean {
         return BREEDING_INGREDIENT.test(stack)
@@ -90,6 +157,9 @@ class TurkeyEntity(entityType: EntityType<out TurkeyEntity>, world: World) :
                 .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 1.0)
                 .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 12.0)
         }
+
+        private val STUFFING_LEVEL: TrackedData<Int> =
+            DataTracker.registerData(TurkeyEntity::class.java, TrackedDataHandlerRegistry.INTEGER)
 
         val BREEDING_INGREDIENT: Ingredient = Ingredient.fromTag(ItemTags.VILLAGER_PLANTABLE_SEEDS)
     }
