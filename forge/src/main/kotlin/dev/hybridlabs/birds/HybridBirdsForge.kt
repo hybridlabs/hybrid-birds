d62d88a129e31d08825b92cad1358c0b98058366
Kevin Murphy <kevin@eorbit.net>
2025-08-23 13:44:13 -0700

    Add skeletal Forge mod entrypoint.

---
+++ forge/src/main/kotlin/dev/hybridlabs/birds/HybridBirdsForge.kt
@@ -0,0 +1,11 @@
+package dev.hybridlabs.birds
+
+import net.minecraftforge.fml.common.Mod
+import dev.hybridlabs.birds.entity.HybridBirdsEntityTypes
+
+@Mod(Constants.MOD_ID)
+class HybridBirdsForge {
+    init {
+        HybridBirdsEntityTypes
+    }
+}
