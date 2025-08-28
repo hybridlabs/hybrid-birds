93a9ce807549c68cff29df831cb55a9ee9bd724e
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2025-08-28 18:03:31 +0300

    Fixed mixin refmap for Forge not working

--- common/src/main/java/dev/hybridlabs/birds/mixin/ChickenEntityMixin.java
+++ common/src/main/java/dev/hybridlabs/birds/mixin/ChickenEntityMixin.java
@@ -9,19 +9,18 @@ import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.animal.*;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Chicken.class)
 public abstract class ChickenEntityMixin extends Animal {
-    protected ChickenEntityMixin(EntityType<? extends Animal> type, Level level) {
+    private ChickenEntityMixin(EntityType<? extends Animal> type, Level level) {
         super(type, level);
     }
 
     @Inject(method = "registerGoals", at = @At("TAIL"))
-    protected void registerGoals(CallbackInfo ci) {
+    private void registerGoals(CallbackInfo ci) {
         this.goalSelector.addGoal(0, new FollowRoosterGoal((Chicken)(Object)this, 1.0, 4.0F, 8.0F));
         this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Ocelot.class, 8.0F, 1.0, 1.0));
         this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, Fox.class, 8.0F, 1.0, 1.0));
