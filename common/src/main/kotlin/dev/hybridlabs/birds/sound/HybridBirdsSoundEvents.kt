1f0ebcbde7b1322411f45844d210cb573ddfcecd
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-02-11 09:27:16 +0200

    Removed needless sounds

--- common/src/main/kotlin/dev/hybridlabs/birds/sound/HybridBirdsSoundEvents.kt
+++ common/src/main/kotlin/dev/hybridlabs/birds/sound/HybridBirdsSoundEvents.kt
@@ -11,23 +11,14 @@ object HybridBirdsSoundEvents {
     val DUCK_AMBIENT = register("duck_ambient")
     val DUCK_HURT = register("duck_hurt")
     val DUCK_DIE = register("duck_die")
-    val DUCKLING_AMBIENT = register("duckling_ambient")
-    val DUCKLING_HURT = register("duckling_hurt")
-    val DUCKLING_DIE = register("duckling_die")
 
     val GOOSE_AMBIENT = register("goose_ambient")
     val GOOSE_HURT = register("goose_hurt")
     val GOOSE_DIE = register("goose_die")
-    val GOSLING_AMBIENT = register("gosling_ambient")
-    val GOSLING_HURT = register("gosling_hurt")
-    val GOSLING_DIE = register("gosling_die")
 
     val SWAN_AMBIENT = register("swan_ambient")
     val SWAN_HURT = register("swan_hurt")
     val SWAN_DIE = register("swan_die")
-    val CYGNET_AMBIENT = register("cygnet_ambient")
-    val CYGNET_HURT = register("cygnet_hurt")
-    val CYGNET_DIE = register("cygnet_die")
 
     val ROOSTER_CALL = register("rooster_call")
     val ROOSTER_AMBIENT = register("rooster_ambient")
@@ -41,16 +32,10 @@ object HybridBirdsSoundEvents {
     val TURKEY_AMBIENT = register("turkey_ambient")
     val TURKEY_HURT = register("turkey_hurt")
     val TURKEY_DIE = register("turkey_die")
-    val POULT_AMBIENT = register("poult_ambient")
-    val POULT_HURT = register("poult_hurt")
-    val POULT_DIE = register("poult_die")
 
     val PEACOCK_AMBIENT = register("peacock_ambient")
     val PEACOCK_HURT = register("peacock_hurt")
     val PEACOCK_DIE = register("peacock_die")
-    val PEACHICK_AMBIENT = register("peachick_ambient")
-    val PEACHICK_HURT = register("peachick_hurt")
-    val PEACHICK_DIE = register("peachick_die")
 
     private fun register(id: String): RegistryObject<SoundEvent> {
         val identifier = ResourceLocation(Constants.MOD_ID, id)
