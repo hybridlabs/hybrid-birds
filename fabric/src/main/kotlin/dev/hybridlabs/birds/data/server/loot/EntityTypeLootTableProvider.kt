7255b93cb6ade69863d14c89abcfbd0e99e63aa5
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-06-28 00:27:11 +0300

    Cleanup imports

--- fabric/src/main/kotlin/dev/hybridlabs/birds/data/server/loot/EntityTypeLootTableProvider.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/birds/data/server/loot/EntityTypeLootTableProvider.kt
@@ -8,8 +8,6 @@ import net.minecraft.advancements.critereon.EntityFlagsPredicate
 import net.minecraft.advancements.critereon.EntityPredicate
 import net.minecraft.core.HolderLookup
 import net.minecraft.resources.ResourceKey
-import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.entity.EntityType
 import net.minecraft.world.item.Items
 import net.minecraft.world.level.storage.loot.LootPool
 import net.minecraft.world.level.storage.loot.LootTable
