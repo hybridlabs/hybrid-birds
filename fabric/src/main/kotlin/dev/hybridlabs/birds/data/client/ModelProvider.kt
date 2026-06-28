55f7e7883d554a9c76cf79cc748bd3782a65497c
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-06-28 15:10:47 +0300

    Spawn egg textures, credit to L2

--- fabric/src/main/kotlin/dev/hybridlabs/birds/data/client/ModelProvider.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/birds/data/client/ModelProvider.kt
@@ -1,29 +1,29 @@
 package dev.hybridlabs.birds.data.client
 
-import dev.hybridlabs.birds.data.HBDataGenerator.filterHybridBirds
 import dev.hybridlabs.birds.item.HBItems
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider
-import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.data.models.BlockModelGenerators
 import net.minecraft.data.models.ItemModelGenerators
-import net.minecraft.data.models.model.ModelLocationUtils
 import net.minecraft.data.models.model.ModelTemplates
-import net.minecraft.world.item.SpawnEggItem
 
 class ModelProvider(output: FabricDataOutput) : FabricModelProvider(output) {
     override fun generateBlockStateModels(generator: BlockModelGenerators) {
-        // spawn eggs
-        BuiltInRegistries.ITEM
-            .filter(filterHybridBirds(BuiltInRegistries.ITEM))
-            .forEach { item ->
-                if (item is SpawnEggItem) {
-                    generator.delegateItemModel(item, ModelLocationUtils.decorateItemModelLocation("template_spawn_egg"))
-                }
-            }
     }
 
     override fun generateItemModels(generator: ItemModelGenerators) {
+        generator.generateFlatItem(HBItems.ROOSTER_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.DUCK_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.GOOSE_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.SWAN_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.TURKEY_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.PEACOCK_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.GUINEA_FOWL_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.PUFFIN_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.SEAGULL_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.PELICAN_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+        generator.generateFlatItem(HBItems.ALBATROSS_SPAWN_EGG.get(), ModelTemplates.FLAT_ITEM)
+
         generator.generateFlatItem(HBItems.DUCK_EGG.get(), ModelTemplates.FLAT_ITEM)
         generator.generateFlatItem(HBItems.GOOSE_EGG.get(), ModelTemplates.FLAT_ITEM)
         generator.generateFlatItem(HBItems.SWAN_EGG.get(), ModelTemplates.FLAT_ITEM)
